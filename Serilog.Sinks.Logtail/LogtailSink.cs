6f424cfb15cf394d641929b11396f2bdaf5f35a4
Vince ten Have <vincetenhave@live.nl>
2023-03-15 12:16:07 +0100

    using HttpClient directly

--- Serilog.Sinks.Logtail/LogtailSink.cs
+++ Serilog.Sinks.Logtail/LogtailSink.cs
@@ -1,33 +1,29 @@
 ﻿using System;
 using System.Linq;
-using Logtail;
 using Serilog.Configuration;
 using Serilog.Core;
 using Serilog.Events;
+using System.Net.Http;
+using System.Net.Mime;
+using Newtonsoft.Json;
+using System.Text;
+using System.Collections.Generic;
+using Serilog.Formatting.Json;
 
 namespace Serilog.Sinks.Logtail
 {
     public class LogtailSink : ILogEventSink, IDisposable
     {
         readonly IFormatProvider? _formatProvider;
-        readonly Drain _logtail;
+        readonly HttpClient _client;
 
         public LogtailSink(IFormatProvider? formatProvider, string sourceToken)
         {
             _formatProvider = formatProvider;
             SourceToken = sourceToken;
-
-            var client = new Client(
-                SourceToken,
-                endpoint: Endpoint,
-                retries: Retries
-            );
-
-            _logtail = new Drain(
-                client,
-                period: TimeSpan.FromMilliseconds(FlushPeriodMilliseconds),
-                maxBatchSize: MaxBatchSize
-            );
+            _client= new HttpClient();
+            //_client.BaseAddress= new Uri("https://in.logtail.com/");
+            _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {sourceToken}");
         }
 
         public string Endpoint { get; set; } = "https://in.logtail.com";
@@ -42,23 +38,23 @@ namespace Serilog.Sinks.Logtail
 
         public void Dispose()
         {
-            _logtail?.Stop().Wait();
+            
         }
 
         public void Emit(LogEvent logEvent)
         {
-            var contextDictionary = logEvent.Properties.ToDictionary(x => x.Key, x => (object)x);
-
-            var log = new global::Logtail.Log()
+			Dictionary<string, string> contextDictionary = new Dictionary<string, string>
+			{
+				{ "message", logEvent.RenderMessage() },
+			};
+            foreach (var key in logEvent.Properties)
             {
-                Timestamp = logEvent.Timestamp,
-                Message = logEvent.RenderMessage(_formatProvider),
-                Level = logEvent.Level.ToString(),
-                Context = contextDictionary,
-            };
-
-            _logtail.Enqueue(log);
-        }
+                contextDictionary.Add(key.Key, key.Value.ToString());
+            }
+			var json = JsonConvert.SerializeObject(contextDictionary);
+			var content = new StringContent(json, Encoding.UTF8, "application/json");
+            _client.PostAsync("https://in.logtail.com/", content);
+		}
     }
 
     public static class LogtailSeqSinkExtensions
